caeec11a737e4d6e52c7e2ecdbc7a7896f9a4fba
ushathapaa <you@example.com>
2024-04-12 18:39:03 +0530

    academic year

---
+++ src/data/themeData.js
@@ -0,0 +1,29 @@
+const darkPalette = {
+  primary: {
+    main: "#196838", // Blue for light mode
+  },
+  secondary: {
+    main: "#2c4c71", // Pink for light mode
+  },
+  mode: "dark",
+};
+
+const lightPalette = {
+  primary: {
+    main: "#196838", // Light Blue for dark mode
+  },
+  secondary: {
+    main: "#446285", // Light Pink for dark mode
+  },
+  mode: "light",
+};
+
+const shapeProperties = {
+  borderRadius: 5,
+};
+
+export default {
+  lightPalette,
+  darkPalette,
+  shapeProperties,
+};
--- src/pages/Administrator/AcademicYear.jsx
+++ src/pages/Administrator/AcademicYear.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import PageHeader from "../../components/PageHeader";
+import CustomTable from "../../components/Tables/CustomTable";
 
 export default function AcademicYear() {
-  return <div>AcademicYear</div>;
+  return (
+    <>
+      <PageHeader title="Academic Year" />
+      {/* <CustomTable
+        actions={["edit"]}
+        bodyDataModal="role"
+        bodyData={roles}
+        tableKeys={roleTableKeys}
+        onEditClick={handleRoleEditClick}
+        module="Roles and Permission"
+      /> */}
+    </>
+  );
 }
