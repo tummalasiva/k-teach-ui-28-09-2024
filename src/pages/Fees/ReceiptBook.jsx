9d42b3261e0bb51aa4d12ca4c15895daa25a89fc
ushathapaa <you@example.com>
2024-04-19 11:41:27 +0530

    riept book

--- src/pages/Fees/ReceiptBook.jsx
+++ src/pages/Fees/ReceiptBook.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import PageHeader from "../../components/PageHeader";
+import { reconciliationTableKeys } from "../../data/tableKeys/reconciliationData";
+import CustomTable from "../../components/Tables/CustomTable";
 
 export default function ReceiptBook() {
+  const [data, setData] = useState([]);
   return (
     <>
       <PageHeader title="Fee Reconciliation" />
+      <CustomTable
+        actions={[]}
+        bodyDataModal="Fee Reconciliation"
+        bodyData={data}
+        tableKeys={reconciliationTableKeys}
+      />
     </>
   );
 }
