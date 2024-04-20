1e1d5b2789b81c827d9f27907e07f5f39d8a3562
Leelakshig <leelakshi.g@webspruce.com>
2024-04-20 21:02:14 +0530

    form

--- src/theme-one/components/BirthdayEvents/RegistrationForm.jsx
+++ src/theme-one/components/BirthdayEvents/RegistrationForm.jsx
@@ -1,14 +1,19 @@
-import { Box, Button, Typography, styled } from "@mui/material";
+import {
+  Box,
+  Button,
+  TextField,
+  Typography,
+  styled,
+  Checkbox,
+  FormControlLabel,
+} from "@mui/material";
 import React, { useState } from "react";
 
-const Wrapper = styled("Box")(() => ({
+const Wrapper = styled(Box)(() => ({
   position: "relative",
+  maxWidth: "480px",
 
   width: "100%",
-  backgroundColor: "#fff",
-
-  borderRadius: "5px",
-  boxShadow: "0 5px 10px rgba(0, 0, 0, 0.2)",
 }));
 
 const Header = styled(Typography)(() => ({
@@ -17,12 +22,22 @@ const Header = styled(Typography)(() => ({
   fontWeight: 600,
   color: "#333",
 }));
+const StyledTextField = styled(TextField)({
+  // boxShadow: "0 5px 10px rgba(0, 0, 0, 0.2)",
+  transition: "box-shadow 0.3s",
+  "&:hover": {
+    boxShadow: "0 5px 10px rgba(0, 0, 0, 0.2)",
+  },
+});
 
 export default function RegistrationForm() {
+  const [rememberMe, setRememberMe] = useState(false);
+
   const [formData, setFormData] = useState({
     name: "",
     email: "",
     message: "",
+    rememberMe,
   });
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -30,58 +45,71 @@ export default function RegistrationForm() {
 
   return (
     <Wrapper>
-      <Header>Registration Form</Header>
+      <Box
+        sx={{ backgroundColor: "#fff", borderRadius: "6px", padding: "34px" }}
+      >
+        <Header textAlign="start" mb={4}>
+          Registration Form
+        </Header>
 
-      <form>
-        <div className="input-box">
-          <input
-            type="text"
+        <form>
+          <StyledTextField
+            variant="outlined"
             name="name"
+            fullWidth
             value={formData.name}
             onChange={handleChange}
             placeholder="Enter your name"
             required
+            sx={{ marginBottom: 3 }}
           />
-        </div>
 
-        <div className="input-box">
-          <input
-            type="email"
+          <StyledTextField
+            fullWidth
+            variant="outlined"
             name="email"
             value={formData.email}
             onChange={handleChange}
             placeholder="Enter your email"
             required
+            sx={{ marginBottom: 3 }}
           />
-        </div>
 
-        <div className="input-box">
-          <input
-            type="text"
+          <StyledTextField
+            fullWidth
+            variant="outlined"
             name="message"
             value={formData.message}
             onChange={handleChange}
             placeholder="Message"
             required
+            sx={{ marginBottom: 1 }}
           />
-        </div>
 
-        <Box className="policy">
-          <input type="checkbox" />
+          <FormControlLabel
+            control={<Checkbox size="small" />}
+            type="checkbox"
+            onChange={(e) => {
+              setRememberMe(e.target.checked);
+            }}
+            sx={{ fontSize: "12px" }}
+            label="I accept all terms & condition"
+          />
 
-          <Typography>I accept all terms & condition</Typography>
-        </Box>
+          <Button
+            fullWidth
+            sx={{ marginTop: 4, padding: 1.5 }}
+            variant="contained"
+            type="submit"
+          >
+            Register Now
+          </Button>
 
-        <div className="input-box button">
-          <Button type="submit">Register Now</Button>
-        </div>
-
-        <div className="text">
-          <Typography>
+          <Typography mt={2} textAlign="center">
             Already have an account? <a href="/login">Login now</a>
           </Typography>
-        </div>
-      </form>
+        </form>
+      </Box>
     </Wrapper>
   );
 }
