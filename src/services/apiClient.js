b0ec21be644ed122307c99c8a4ee4c32aa1ebf64
gnavneet28 <sggauravn@gmail.com>
2024-04-25 19:54:12 +0530

    update apiClient with refresh tokens

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -16,6 +16,22 @@ const setAuthToken = (config) => {
   return config;
 };
 
+const refreshAccessToken = async () => {
+  try {
+    const res = await client.post("/refresh-token");
+    const newAccessToken = res.data.accessToken;
+    // Store new access token
+    window.localStorage.setItem("access_token", newAccessToken);
+    return newAccessToken;
+  } catch (error) {
+    // Handle error (e.g., redirect to login)
+    console.log("refresh token error", error);
+    window.localStorage.removeItem("access_token");
+    window.localStorage.removeItem("current_user");
+    window.location.href = "/login";
+  }
+};
+
 client.interceptors.request.use(setAuthToken, (error) => Promise.reject(error));
 
 client.interceptors.response.use(
@@ -26,16 +42,19 @@ client.interceptors.response.use(
     }
     return response;
   },
-  (error) => {
+  async (error) => {
     const { response } = error;
+    const originalRequest = error.config;
     if (response && response.status === 401) {
-      window.localStorage.removeItem("access_token");
-      window.localStorage.removeItem("current_inventory_user");
-      window.location.href = "/";
+      originalRequest._retry = true;
+      const accessToken = await refreshAccessToken();
+      originalRequest.headers["x-auth-token"] = `bearer ${accessToken}`;
+      return Axios(originalRequest);
     }
     if (response && response.data && response.data.message) {
       toast.error(response.data.message);
     } else {
+      console.log(error);
       toast.error("An error occurred.");
     }
     return Promise.reject(error);
