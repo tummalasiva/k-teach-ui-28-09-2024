6678198bc16e1e5fe684393f9d1406861a68ae83
gnavneet28 <sggauravn@gmail.com>
2024-05-10 11:31:14 +0530

    updated setting

--- src/pages/ManageInstitute/ManageInstitute.jsx
+++ src/pages/ManageInstitute/ManageInstitute.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import PageHeader from "../../components/PageHeader";
 import CustomTable from "../../components/Tables/CustomTable";
 import { manageInstituteTableKeys } from "../../data/tableKeys/manageInstituteData";
 import AddForm from "../../forms/AddForm";
 import { useNavigate } from "react-router-dom";
+import { get } from "../../services/apiMethods";
+import { PRIVATE_URLS } from "../../services/urlConstants";
 
 export default function ManageInstitute() {
   const [data, setData] = useState();
@@ -11,6 +13,19 @@ export default function ManageInstitute() {
   const handleAddClick = (e) => {
     navigate("/sch/manage-institute/add-institute");
   };
+
+  const getData = async () => {
+    try {
+      const { data } = await get(PRIVATE_URLS.school.list);
+      setData(data.result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <>
       <PageHeader title="Manage Institute" />
@@ -19,6 +34,7 @@ export default function ManageInstitute() {
         bodyDataModal="manage institute"
         bodyData={data}
         tableKeys={manageInstituteTableKeys}
+        toggleStatus="active"
       />
       <AddForm title="Add Institute" onAddClick={handleAddClick} />
     </>
