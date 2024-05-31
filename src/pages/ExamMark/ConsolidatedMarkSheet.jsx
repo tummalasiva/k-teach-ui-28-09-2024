3cd0a7357118b4eb9ba2e1ea2384bef1cb01000a
ushathapaa <you@example.com>
2024-05-31 11:19:25 +0530

    help desk form

--- src/pages/ExamMark/ConsolidatedMarkSheet.jsx
+++ src/pages/ExamMark/ConsolidatedMarkSheet.jsx
@@ -2,7 +2,7 @@
 
 import React, { useContext, useEffect, useState } from "react";
 import { useFormik } from "formik";
-import { Button, Grid, Paper } from "@mui/material";
+import { Button, Grid, IconButton, Paper } from "@mui/material";
 import { consolidateMarksTableKeys } from "../../data/tableKeys/consolidateMarksCardData";
 import PageHeader from "../../components/PageHeader";
 import FormSelect from "../../forms/FormSelect";
@@ -10,12 +10,16 @@ import CustomTable from "../../components/Tables/CustomTable";
 import { PRIVATE_URLS } from "../../services/urlConstants";
 import { get } from "../../services/apiMethods";
 import SettingContext from "../../context/SettingsContext";
+import FormInput from "../../forms/FormInput";
+import { Close } from "@mui/icons-material";
 
 export default function ConsolidatedMarkSheet() {
   const [data, setData] = useState([]);
   const { selectedSetting } = useContext(SettingContext);
   const [classes, setClasses] = useState([]);
   const [section, setSection] = useState([]);
+  const [consolidated, setConsolidated] = useState([]);
+
   const getClass = async () => {
     try {
       const { data } = await get(PRIVATE_URLS.class.list, {
@@ -64,6 +68,24 @@ export default function ConsolidatedMarkSheet() {
       getSection();
     }
   }, [entryFormik.values.class, selectedSetting]);
+
+  const handleRemoveSubject = (examIndex, subjectIndex) => {
+    setConsolidated(
+      consolidated.map((con, i) => {
+        if (i === examIndex) {
+          return {
+            ...con,
+            subjects: con.subjects.filter(
+              (subject, index) => index !== subjectIndex
+            ),
+          };
+        } else {
+          return con;
+        }
+      })
+    );
+  };
+
   return (
     <>
       <PageHeader title="Consolidate Marks Sheet" />
@@ -93,6 +115,57 @@ export default function ConsolidatedMarkSheet() {
             <Button size="small" variant="contained">
               Add exams
             </Button>
+          </Grid>
+          <Grid xs={12} md={12} lg={12} item display="flex" gap={2}>
+            <Grid xs={12} md={6} lg={3} item>
+              <FormInput
+                required={true}
+                name="compress"
+                formik={entryFormik}
+                label="Compress"
+              />
+            </Grid>
+            <Grid xs={12} md={6} lg={3} item>
+              <FormSelect
+                required={true}
+                name="exam"
+                formik={entryFormik}
+                label="Select Exam"
+                // options={""}
+              />
+            </Grid>
+            <Grid xs={12} md={6} lg={3} item display="flex" alignItems="center">
+              <IconButton color="error">
+                {/* <Button onClick={() => handleRemoveSubject(index, subjectIndex)}> */}
+                <Close />
+              </IconButton>
+              <Button>Add Subject</Button>
+            </Grid>
+          </Grid>
+          <Grid xs={12} md={12} lg={12} item display="flex" gap={2}>
+            <Grid xs={12} md={6} lg={3} item>
+              <FormInput
+                required={true}
+                name="compress"
+                formik={entryFormik}
+                label="Compress"
+              />
+            </Grid>
+            <Grid xs={12} md={6} lg={3} item>
+              <FormSelect
+                required={true}
+                name="subject"
+                formik={entryFormik}
+                label="Select Subject"
+                // options={""}
+              />
+            </Grid>
+            <Grid xs={12} md={6} lg={3} item display="flex" alignItems="center">
+              <IconButton color="error">
+                {/* <Button onClick={() => handleRemoveSubject(index, subjectIndex)}> */}
+                <Close />
+              </IconButton>
+            </Grid>
           </Grid>
         </Grid>
       </Paper>
--- src/pages/HelpDesk.jsx
+++ src/pages/HelpDesk.jsx
@@ -1,10 +1,62 @@
+/** @format */
+
 import React, { useState } from "react";
 import PageHeader from "../components/PageHeader";
 import CustomTable from "../components/Tables/CustomTable";
 import { ticketTableKeys } from "../data/tableKeys/ticketData";
+import AddForm from "../forms/AddForm";
+import FileSelect from "../forms/FileSelect";
+import { useFormik } from "formik";
+import { Grid } from "@mui/material";
+import FormModal from "../forms/FormModal";
+import FormInput from "../forms/FormInput";
 
 export default function HelpDesk() {
   const [data, setData] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [reportImg, setReportImg] = useState([]);
+  const [dataToEdit, setDataToEdit] = useState(null);
+
+  const handelExamTerm = () => {
+    setOpen(true);
+  };
+
+  const entryFormik = useFormik({
+    initialValues: {
+      title: dataToEdit?.title || "",
+      examType: dataToEdit?.examType || "",
+      marksAssignmentAllowed: dataToEdit?.marksAssignmentAllowed || false,
+      note: dataToEdit?.note || "",
+      isPublic: dataToEdit?.isPublic || false,
+    },
+    onSubmit: console.log("lll"),
+    enableReinitialize: true,
+  });
+
+  const handleChangeFiles = (e, index) => {
+    const { files } = e.target;
+    let fileList = [];
+    if (files.length > 0) {
+      for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+        fileList.push(file);
+      }
+      setReportImg(fileList);
+    } else {
+      console.log("No files selected");
+    }
+  };
+
+  const handleRemoveFile = (fileName, index) => {
+    setReportImg(reportImg.filter((img) => img.name != fileName));
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setDataToEdit(null);
+  };
+
   return (
     <>
       <PageHeader title="Tickets" />
@@ -14,6 +66,46 @@ export default function HelpDesk() {
         bodyData={data}
         tableKeys={ticketTableKeys}
       />
+
+      <AddForm title="Add Tickets" onAddClick={handelExamTerm} />
+
+      <FormModal
+        open={open}
+        formik={entryFormik}
+        formTitle={dataToEdit ? "Update Report Problem" : "Add Report Problem"}
+        onClose={handleClose}
+        submitButtonTitle={dataToEdit ? "Update" : "Submit"}
+        adding={loading}>
+        <Grid rowSpacing={0} columnSpacing={2} container>
+          <Grid xs={12} sm={6} md={6} item>
+            <FormInput
+              formik={entryFormik}
+              name="title"
+              label="Subject"
+              required={true}
+            />
+          </Grid>
+          <Grid xs={12} sm={6} md={6} item>
+            <FormInput
+              formik={entryFormik}
+              name="description"
+              label="Description"
+              required={true}
+            />
+          </Grid>
+          <Grid xs={12} sm={6} md={6} item>
+            <FileSelect
+              name="receipt"
+              label="Select Files"
+              onChange={(e) => handleChangeFiles(e)}
+              customOnChange={true}
+              selectedFiles={reportImg}
+              onRemove={(fileName) => handleRemoveFile(fileName)}
+              multi={true}
+            />
+          </Grid>
+        </Grid>
+      </FormModal>
     </>
   );
 }
