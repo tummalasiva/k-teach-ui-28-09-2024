4c9e05412f9919d634891131fc17859f9c022a9b
gnavneet <gauravkumar.navneet@webspruce.com>
2024-06-07 09:59:58 +0530

    updated client

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -7,12 +7,11 @@ import "react-toastify/dist/ReactToastify.css";
 export const API_PREFIX = process.env.REACT_APP_API_PREFIX;
 
 const client = Axios.create({
-  baseURL:
-    "https://101e-2404-ca40-fa02-3f00-acb2-e9de-8f24-3243.ngrok-free.app/ecamps/v1",
+  baseURL: " https://1b8e-117-198-255-145.ngrok-free.app/ecamps/v1",
   headers: {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin":
-      "https://101e-2404-ca40-fa02-3f00-acb2-e9de-8f24-3243.ngrok-free.app",
+      " https://1b8e-117-198-255-145.ngrok-free.app",
   },
 });
 
