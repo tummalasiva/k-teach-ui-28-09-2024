385264af0d8402483257ea2b408f078a8b973926
Leelakshig <leelakshi.g@webspruce.com>
2024-07-15 09:58:27 +0530

    bugs fix

--- src/components/Dashboard/Attendance.jsx
+++ src/components/Dashboard/Attendance.jsx
@@ -107,7 +107,11 @@ export default function Attendance() {
   return (
     <>
       <Paper
-        sx={{ padding: 2, margin: "20px 0px", backgroundColor: "whitesmoke" }}>
+        sx={{
+          padding: "0px 15px",
+          margin: "20px 0px",
+          backgroundColor: "whitesmoke",
+        }}>
         <Grid container spacing={2}>
           <Grid item xs={12} sm={12} md={4} lg={4}>
             <OuterCard>
