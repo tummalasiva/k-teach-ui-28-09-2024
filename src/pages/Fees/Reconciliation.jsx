3e1a9254f92eb6c3a410662b9a2afd00b10f1df2
gnavneet <gauravkumar.navneet@webspruce.com>
2024-07-19 17:21:15 +0530

    Tested reconciliation

--- src/pages/Fees/Reconciliation.jsx
+++ src/pages/Fees/Reconciliation.jsx
@@ -1,10 +1,36 @@
-import React, { useState } from "react";
+/** @format */
+
+import React, { useContext, useEffect, useState } from "react";
 import PageHeader from "../../components/PageHeader";
 import CustomTable from "../../components/Tables/CustomTable";
 import { reconciliationTableKeys } from "../../data/tableKeys/reconciliationData";
+import { get } from "../../services/apiMethods";
+import { PRIVATE_URLS } from "../../services/urlConstants";
+import SettingContext from "../../context/SettingsContext";
 
 export default function Reconciliation() {
+  const { selectedSetting } = useContext(SettingContext);
   const [data, setData] = useState([]);
+
+  const getAllReceipts = async () => {
+    try {
+      const { data } = await get(
+        PRIVATE_URLS.receipt.getReceiptsPaidWithCheque,
+        {
+          params: {
+            schoolId: selectedSetting._id,
+          },
+        }
+      );
+      setData(data.result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getAllReceipts();
+  }, [selectedSetting._id]);
   return (
     <>
       <PageHeader title="Fee Reconciliation" />
