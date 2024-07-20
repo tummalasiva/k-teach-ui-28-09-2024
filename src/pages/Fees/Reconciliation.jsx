6da60e4d6ed42ba7b5238fb6355420eab9602c4e
ushathapaa <you@example.com>
2024-07-20 13:35:23 +0530

    reconciliationstation update

--- src/pages/Fees/Reconciliation.jsx
+++ src/pages/Fees/Reconciliation.jsx
@@ -4,9 +4,65 @@ import React, { useContext, useEffect, useState } from "react";
 import PageHeader from "../../components/PageHeader";
 import CustomTable from "../../components/Tables/CustomTable";
 import { reconciliationTableKeys } from "../../data/tableKeys/reconciliationData";
-import { get } from "../../services/apiMethods";
+import { get, put } from "../../services/apiMethods";
 import { PRIVATE_URLS } from "../../services/urlConstants";
 import SettingContext from "../../context/SettingsContext";
+// icons
+import CheckIcon from "@mui/icons-material/Check";
+import CloseIcon from "@mui/icons-material/Close";
+import { IconButton, Stack, Tooltip } from "@mui/material";
+
+const CustomActionFee = ({
+  onUpdate = () => {},
+  data = {},
+  // onEditClick = () => {},
+  onNavigateFeeMap = () => {},
+}) => {
+  const [loading, setLoading] = useState(false);
+  console.log(data?.reconciliationStatus, "llloosing");
+
+  const updateStatus = async (status, id) => {
+    console.log(status, id, "pooo");
+
+    try {
+      setLoading(true);
+      const payload = {
+        action: status,
+      };
+      await put(
+        `${PRIVATE_URLS.receipt.updateSingleReceiptReconciliationStatusStatus}/${id}`,
+        payload
+      );
+      onUpdate();
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+  };
+
+  return (
+    <>
+      <Stack direction="row" spacing={1} alignItems="center">
+        <Tooltip title="Approve">
+          <IconButton
+            size="small"
+            onClick={() => updateStatus("Approve", data?._id)}>
+            <CheckIcon color="primary" fontSize="12px" />
+          </IconButton>
+        </Tooltip>
+        {data?.reconciliationStatus === "Rejected" && (
+          <Tooltip
+            title="Reject"
+            onClick={() => updateStatus("Reject", data?._id)}>
+            <IconButton size="small">
+              <CloseIcon color="error" fontSize="12px" />
+            </IconButton>
+          </Tooltip>
+        )}
+      </Stack>
+    </>
+  );
+};
 
 export default function Reconciliation() {
   const { selectedSetting } = useContext(SettingContext);
@@ -41,14 +97,17 @@ export default function Reconciliation() {
   useEffect(() => {
     getAllReceipts();
   }, [selectedSetting._id]);
+
   return (
     <>
       <PageHeader title="Fee Reconciliation" />
       <CustomTable
-        actions={[]}
+        actions={["custom"]}
         bodyDataModal="Fee Reconciliation"
         bodyData={data}
         tableKeys={reconciliationTableKeys}
+        CustomAction={CustomActionFee}
+        onUpdate={getAllReceipts}
       />
     </>
   );
