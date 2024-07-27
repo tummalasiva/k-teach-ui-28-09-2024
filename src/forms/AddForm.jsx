98ff439f41fe051e2fa6cbe4c5bb00b985f44ef8
ushathapaa <you@example.com>
2024-07-27 12:07:39 +0530

    checkpermissions roledata add

--- src/forms/AddForm.jsx
+++ src/forms/AddForm.jsx
@@ -1,35 +1,43 @@
+/** @format */
+
 import { Box, Fab, Tooltip } from "@mui/material";
 import React from "react";
 
 import AddIcon from "@mui/icons-material/Add";
 import themeData from "../data/themeData";
-export default function AddForm({ disabled, title, onAddClick = () => {} }) {
+import CheckPermission from "../components/Authentication/CheckPermission";
+export default function AddForm({
+  disabled,
+  title,
+  module = "",
+  onAddClick = () => {},
+}) {
   return (
     <>
-      <Box
-        sx={{
-          position: "fixed",
-          width: "-moz-fit-content",
-          width: "fit-content",
-          right: "70px",
-          bottom: "20px",
-        }}
-      >
-        <Tooltip title={title}>
-          <Fab
-            variant="contained"
-            onClick={onAddClick}
-            disabled={disabled}
-            sx={{
-              color: "#ffff",
-              background: themeData.darkPalette.primary.main,
-              ":hover": { background: themeData.darkPalette.primary.main },
-            }}
-          >
-            <AddIcon />
-          </Fab>
-        </Tooltip>
-      </Box>
+      <CheckPermission module={module} permission="add">
+        <Box
+          sx={{
+            position: "fixed",
+            width: "-moz-fit-content",
+            width: "fit-content",
+            right: "70px",
+            bottom: "20px",
+          }}>
+          <Tooltip title={title}>
+            <Fab
+              variant="contained"
+              onClick={onAddClick}
+              disabled={disabled}
+              sx={{
+                color: "#ffff",
+                background: themeData.darkPalette.primary.main,
+                ":hover": { background: themeData.darkPalette.primary.main },
+              }}>
+              <AddIcon />
+            </Fab>
+          </Tooltip>
+        </Box>
+      </CheckPermission>
     </>
   );
 }
